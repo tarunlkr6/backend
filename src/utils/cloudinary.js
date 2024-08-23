7cdd154a729df8f481a10a88cce32612cf5ee37d
Animus <tarunkumarlkr6@gmail.com>
2024-08-23 22:11:52 +0530

    added file upload method in cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
@@ -6,3 +6,17 @@ cloudinary.config({
     api_key: process.env.LOUDINARY_API_KEY,
     api_seceret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (path) => {
+    try {
+        if (!path) { return null }
+        // upload file on cloudinary
+        const response = await cloudinary.v2.uplaoder.upload(path, { resource_type: "auto" })
+        // uploaded successfully
+        console.log("File uploaded successfully ", response.url);
+        return response
+    } catch (err) {
+        fs.unlinkSync(path); // remove the temporary file when the upload operation failed
+        return null;
+    }
+}
