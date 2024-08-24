cae49e3b5ad183582c1fbfc2b1d4a9c0fd3831bf
Animus <tarunkumarlkr6@gmail.com>
2024-08-25 02:27:35 +0530

    bug fixes in cloudinary.js and added coverImage validation

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,18 +4,17 @@ import fs from "fs";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_seceret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (path) => {
-    console.log(path);
     try {
-        //if (!path) return null;
+        if (!path) { return null }
         // upload file on cloudinary
         const response = await cloudinary.uploader.upload(path, { resource_type: "auto" })
-        console.log(response)
         // uploaded successfully
-        console.log("File uploaded successfully ", response.url);
+        //console.log("File uploaded successfully ", response.url);
+        fs.unlinkSync(path)
         return response;
     } catch (error) {
         fs.unlinkSync(path); // remove the temporary file when the upload operation failed
